b852292db10b5e28c20f11618189e951a2c3bc98
Pasi Niemi <pasi.niemi@nitorcreations.com>
2019-09-26 17:06:31 +0300

    Fix harmonized variable

--- vend.sh
+++ vend.sh
@@ -60,7 +60,7 @@ fi
 
 vend_when_approved() {
   # https://developer.github.com/v3/pulls/reviews/#list-reviews-on-a-pull-request
-  body=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/${GITHUB_REPOSITORY}/pulls/${number}/reviews?per_page=100")
+  body=$(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${URI}/repos/${GITHUB_REPOSITORY}/pulls/${NUMBER}/reviews?per_page=100")
   echo "$body"
   reviews=$(echo "$body" | jq --raw-output '.[] | {state: .state} | @base64')
 
